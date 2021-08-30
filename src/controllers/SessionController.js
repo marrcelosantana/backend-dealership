e5361584e2f844145a2c93053d4cacae4b456b56
marrcelosantana <marcelosantana@alu.ufc.br>
2021-08-30 00:38:59 -0300

    add remove function on clients

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,13 +3,10 @@ const connection = require('../database/connection');
 module.exports = {
   async create(request, response){
     const { id } = request.body;
-
     const client = await connection('clients').where('id', id).select('name').first();
-
     if(!client){
       return response.status(400).json({ error: "Não existe um cliente com esse ID" });
     }
-
     return response.json(client);
   }
 }
